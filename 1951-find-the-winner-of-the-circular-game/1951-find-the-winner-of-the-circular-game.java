6df7d474b394d1595cbd4dca076c720d73675222
Ritesh Bhandari <85375196+riteshhbhandari@users.noreply.github.com>
2024-07-17 18:40:40 +0530

    Time: 74 ms (5.01%), Space: 44.3 MB (11.8%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -5,10 +5,9 @@ class Solution {
             arr.add(a+1);
         }
         int flg=1;
-        int rem=0;
         while(arr.size()!=1){
             if(flg!=k){
-                rem= arr.remove(0);
+                int rem= arr.remove(0);
                 arr.add(rem);
                 flg++;
             }
