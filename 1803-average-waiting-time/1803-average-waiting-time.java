2f5af96cc1b612293df63c4c6f4b4e1b1de838b1
Ritesh Bhandari <85375196+riteshhbhandari@users.noreply.github.com>
2024-07-20 03:00:52 +0530

    Time: 3 ms (89.64%), Space: 72.6 MB (68.88%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.java
+++ 1803-average-waiting-time/1803-average-waiting-time.java
@@ -1,19 +1,19 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        int n = customers.length;
-        double time_waiting = customers[0][1];
-        int finished_prev = customers[0][0] + customers[0][1];
+        double total_waiting_time = 0;
+        int current_time = 0;
 
-        for (int customer_ind = 1; customer_ind < n; ++customer_ind) {
-            int[] times = customers[customer_ind];
-            int arrive = times[0];
-
-            int start_cook = Math.max(arrive, finished_prev);
-            int end_time = start_cook + times[1];
-            finished_prev = end_time;
-            time_waiting += end_time - arrive;
+        for (int[] customer : customers) {
+            int arrival = customer[0];
+            int service = customer[1];
+            if (current_time < arrival) {
+                current_time = arrival;
+            }
+            int waiting_time = current_time - arrival + service;
+            total_waiting_time += waiting_time;
+            current_time += service;
         }
 
-        return time_waiting / n;
+        return total_waiting_time / customers.length;
     }
 }
