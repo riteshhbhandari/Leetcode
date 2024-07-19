44675278f18c0e18faec55d1a1b3c5d3cb4f6537
Ritesh Bhandari <85375196+riteshhbhandari@users.noreply.github.com>
2024-07-20 02:36:08 +0530

    Time: 17 ms (5.36%), Space: 45.4 MB (19.71%) - LeetHub

---
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        ArrayList <Integer> a1=new ArrayList<>();
+        int r=0, c=0;
+        int rows=matrix.length;
+        int column=matrix[0].length;
+        int flg=0;
+        for(int k=0;k<rows;k++){
+            flg=0;
+            int min=matrix[k][0];
+            for(int i=0;i<column;i++){ 
+                if(min>matrix[k][i]){
+                    min=matrix[k][i];
+                    r=k;
+                    c=i;
+                    System.out.println(matrix[k][i]);
+                }
+            }
+            for(int j=0;j<rows;j++){
+                if(min>=matrix[j][c])
+                {flg++;}
+            }
+            if(flg==rows)
+            {
+                a1.add(min);
+                System.out.println(min);
+                return a1;
+            }
+        }
+        return a1;
+    }
+}
