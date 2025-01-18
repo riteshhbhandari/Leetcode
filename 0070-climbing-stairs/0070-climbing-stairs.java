5932f73911ae44635da699e3b322d7a292cec475
Ritesh Bhandari <85375196+riteshhbhandari@users.noreply.github.com>
2025-01-18 21:34:47 +0530

    Time: 0 ms (100%), Space: 40.7 MB (26.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,43 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[]=new int [n+1];
-        Arrays.fill(dp,-1);
-         climbStairs(n, dp);
-         return dp[n];
-    }
-    private static int climbStairs(int n, int dp[] ){
-        if (n==0 || n==1){
-            dp[n]=1;
-            return 1;
-        }
+    //memoization
+    //     int dp[]=new int [n+1];
+    //     Arrays.fill(dp,-1);
+    //      climbStairs(n, dp);
+    //      return dp[n];
+    // }
+    // private static int climbStairs(int n, int dp[] ){
+    //     if (n==0 || n==1){
+    //         dp[n]=1;
+    //         return 1;
+    //     }
         
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]=climbStairs(n-1, dp) + climbStairs(n-2, dp);
+    //     if(dp[n]!=-1)
+    //         return dp[n];
+
+    //     return dp[n]=climbStairs(n-1, dp) + climbStairs(n-2, dp);
+    // }
+
+    //tabulation
+    // int dp[]=new int [n+1];
+    // dp[0]=dp[1]= 1;
+    // for(int i=2;i<=n;i++)
+    
+    //     dp[i]=dp[i-1]+dp[i-2];
+    // return dp[n];
+
+    //space optimised approach Space complexity O(1)
+    int prev= 1;
+    int prev2=1;
+    int curr=0;
+    if (n==1 || n==0)
+    return 1;
+    for(int i=2;i<=n;i++){
+        curr=prev+prev2;
+        prev2=prev;
+        prev=curr;
     }
+    return curr;
+    
+}
 }
